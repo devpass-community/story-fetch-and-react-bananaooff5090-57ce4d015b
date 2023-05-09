150291844f8198827bf491ebfc391164deb6578e
giovana.borges <gigi12345as17@gmail.com>
2023-05-08 22:18:01 -0300

    :feat add axios and make request api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { useState } from "react";
+import axios from "axios"
 import Container from "./components/Container";
 import Spinner from "./components/Spinner";
 
 function App() {
   const [quote, setQuote] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleClick = async (event) => {
-    setIsLoading(true)
     // TODO
     setIsLoading(false)
+    
+    try {
+      const response = await axios.get('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1');
+      const result = response.data
+      setQuote(result.text)
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
